37bff0bfb0ea9738bbea2949a07003d99e43f72f
Agus Russo <agus.drum12@gmail.com>
2024-11-19 17:14:00 -0300

    add layers

--- neural_network.py
+++ neural_network.py
@@ -7,11 +7,13 @@ class NeuralNetwork(nn.Module):
         self.conv1 = nn.Conv2d(1, 10, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(10, 20, 5)
-        self.fc1 = nn.Linear(320, 10)
+        self.fc1 = nn.Linear(320, 320)
+        self.fc2 = nn.Linear(320, 10)
 
     def forward(self, x):
         x = self.pool(nn.functional.relu(self.conv1(x)))
         x = self.pool(nn.functional.relu(self.conv2(x)))
         x = x.view(-1, 320)
-        x = nn.functional.log_softmax(self.fc1(x), dim=1)
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.log_softmax(self.fc2(x), dim=1)
         return x
