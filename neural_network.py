25841dbd0d7ad95bb92aa615e2f59998df76907b
Agus Russo <agus.drum12@gmail.com>
2024-11-19 16:35:00 -0300

    first commit

---
+++ neural_network.py
@@ -0,0 +1,17 @@
+from torch import nn
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(10, 20, 5)
+        self.fc1 = nn.Linear(320, 10)
+
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 320)
+        x = nn.functional.log_softmax(self.fc1(x), dim=1)
+        return x
